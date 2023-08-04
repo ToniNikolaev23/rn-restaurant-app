65e0ba152cf170a26c0e192cad9fbff3cb169b71
Toni Stoyanov <toni@1forfit.com>
2023-08-04 15:37:31 +0300

    Init project

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={{
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+        <Text style={styles.subtitle}>$ 1.99 15-30 min</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  restaurantContainer: {
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 4 / 3,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: "500",
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: "gray",
   },
 });
